b0bc99db1ee990afc05b6db95c921d9547f3b70d
Jonathan Graham <sendthistojon@gmail.com>
2015-08-29 09:07:10 -0500

    Initial commit

---
+++ main.rb
@@ -0,0 +1,18 @@
+#print "Current Board"
+#print visualization of board
+#put in a function
+#pass a message to print
+
+puts "Current Board"
+
+puts """
+   |   |   
+   |   |   
+___|___|___
+   |   |   
+   |   |   
+___|___|___
+   |   |   
+   |   |   
+   |   |   
+"""
